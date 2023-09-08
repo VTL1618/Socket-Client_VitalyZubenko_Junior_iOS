fb87ab0f7cb194cdc5aa8609e5bca86ad878ec07
VTL1618 <vitalyzubenkowork@gmail.com>
2023-09-08 14:06:38 +0300

    Initial Commit

---
+++ Task2_VitalyZubenko_Junior_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task2_VitalyZubenko_Junior_iOS
+//
+//  Created by Vitaly Zubenko on 08.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
