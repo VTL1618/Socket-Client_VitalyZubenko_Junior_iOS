14912bf6cf8fb6d70d27bfcf45e3bb9e6f515a66
VTL1618 <vitalyzubenkowork@gmail.com>
2023-09-08 15:08:23 +0300

    Basic struct. Start Class SocketRoom - for open/connect/send-receive/close connection. And strong ref from VC to it.

---
+++ Task2_VitalyZubenko_Junior_iOS/SocketRoom.swift
@@ -0,0 +1,28 @@
+//
+//  SocketRoom.swift
+//  Task2_VitalyZubenko_Junior_iOS
+//
+//  Created by Vitaly Zubenko on 08.09.2023.
+//
+
+import UIKit
+
+class SocketRoom: NSObject {
+    
+    var inStream: InputStream!
+    var outStream: OutputStream!
+    
+    var username = ""
+    
+    let maxReadLength = 1000
+    
+    func setNetworkCommunication() {
+        var readStream: Unmanaged<CFReadStream>?
+        var writeStream: Unmanaged<CFWriteStream>?
+        
+        CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault, "localhost" as! CFString, 3000, &readStream, &writeStream)
+        
+        inStream = readStream!.takeRetainedValue()
+        outStream = writeStream!.takeRetainedValue()
+    }
+}
--- Task2_VitalyZubenko_Junior_iOS/SocketRoomViewController.swift
+++ Task2_VitalyZubenko_Junior_iOS/SocketRoomViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class SocketRoomViewController: UIViewController {
 
+    let socketRoom = SocketRoom()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
